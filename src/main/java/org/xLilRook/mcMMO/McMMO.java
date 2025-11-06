d21d997acfc7646ade7fe438c5905a34be4b1562
MatiasStranek <matiasstranek@gmail.com>
2025-11-06 02:09:57 +0100

    Created some structure

--- src/main/java/org/xLilRook/mcMMO/McMMO.java
+++ src/main/java/org/xLilRook/mcMMO/McMMO.java
@@ -4,14 +4,40 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class McMMO extends JavaPlugin {
 
+    private static McMMO instance;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
+        getLogger().info("McMMO Plugin wurde gestartet ✅");
 
+        // Initialisierung
+        registerCommands();
+        registerEvents();
+        loadConfig();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("McMMO Plugin wurde gestoppt ❌");
+    }
+
+    private void registerCommands() {
+        // Meine Commands
+    }
+
+    private void registerEvents() {
+        // Meine Events
+    }
+
+    private void loadConfig() {
+        saveDefaultConfig();
+        getLogger().info("Config geladen.");
+    }
+
+    public static McMMO getInstance() {
+        return instance;
     }
 }
